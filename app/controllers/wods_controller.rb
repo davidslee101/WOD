c6ca86a0834baf69941c1c6d367a9d2a123e1f49
david <davidslee101@gmail.com>
2015-07-22 16:54:33 +0000

    set up index page

---
+++ app/controllers/wods_controller.rb
@@ -0,0 +1,4 @@
+class WodsController < ApplicationController
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 WOD::Application.routes.draw do
+
+  root 'wods#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
