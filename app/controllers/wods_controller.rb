bcbfb4f493a8f22d528d85001c50956e75be1ff1
david <davidslee101@gmail.com>
2015-07-22 18:31:03 +0000

    quiz 2 finished

--- app/controllers/wods_controller.rb
+++ app/controllers/wods_controller.rb
@@ -1,5 +1,20 @@
 class WodsController < ApplicationController
 	def index
-		@wod = Wod.order("RANDCOM()").first
+		@wod = Wod.order("RANDOM()").first
+	end
+
+	def new
+		@wod = Wod.new
+	end
+
+	def create
+		Wod.create(wod_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def wod_params
+		params.require(:wod).permit(:workout)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 WOD::Application.routes.draw do
 
   root 'wods#index'
+  resources :wods
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
